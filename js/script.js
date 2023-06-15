978c7e107644861c9794dd9595119fa0f65ff85f
Gui-HR <guilhermehuertar@gmail.com>
2023-06-15 16:56:40 -0300

    Correção

--- js/script.js
+++ js/script.js
@@ -28,6 +28,7 @@ const resultado = document.querySelector('#resultado')
 // Auxiliares
 var calculo =  []
 var valor = 0
+var quantasContas
  
 for (i in numeros){
     let posicao = i
@@ -83,37 +84,38 @@ function limpaArray () {
 igual.addEventListener('click', () => {
     calculo.push(valor)
     valor = 0
-    console.log(calculo.length);
-    console.log(calculo);
+    calculo = calculo.filter((v) => {
+        return v
+    })
+    quantasContas = (calculo.length / 2) + .5
 
-    for(let i = 0; i <= (calculo.length); i++){
+    for(let i = 0; i <= quantasContas; i++){
+
+        console.log(calculo);
 
         switch (calculo[1]) {
             case '+':
                 calculo[0] = String(Number(calculo[0]) + Number(calculo[2]))
                 limpaArray ()
-                break;
+                break
     
             case '-':
                 calculo[0] = String(Number(calculo[0]) - Number(calculo[2]))
                 limpaArray ()
-                break;
+                break
     
             case 'x':
                 calculo[0] = String(Number(calculo[0]) * Number(calculo[2]))
                 limpaArray ()
-
-                break;
+                break
     
             case '/':
                 calculo[0] = String(Number(calculo[0]) / Number(calculo[2]))
                 limpaArray ()
-                break;
+                break
         
             default:
-                break;
         }
-        console.log(calculo);
     }
 
     resultado.innerHTML = calculo[0]
@@ -122,11 +124,22 @@ igual.addEventListener('click', () => {
 
 /*
     Fazer:
-    - Um jeito de armazenar o valor digitado (não pode ser assim que aperta o botão do dígito, pois o usuário pode querer um número com mais de 1 dígito).
-    - Um jeito para armazenar os sinais das contas, para só realizar as contas quando apertar o '='.
+    - Funcionar usando o teclado.
+    - Arrumar a telinha dos calculos para caber todos os numeros.
+    - Botao de apagar funcionar.
+    - As operações seguirem a ordem correta.
+    - Botao de porcentagem funcionar.
+    - Botao de parenteses funcionar
 
     Ideias:
     - Para fazer o parenteses criar uma variavel auxiliar e usar um if, se o botão for apertado uma vez vai colocar "(" se for apertado pela segunda vez vai colocar ")".
+    - Para o botao de parenteses colocar as contas dentro dele em um array separado, e realizar essas contas antes, depois mandar o resultado para a posição correta.
+    - Para fazer o apagar funcionar criar uma variável auxiliar, que armazena os valores e quando apagar a apaga dessa variavel.
+
+
+    Feito:
+    - Um jeito de armazenar o valor digitado (não pode ser assim que aperta o botão do dígito, pois o usuário pode querer um número com mais de 1 dígito).
+    - Um jeito para armazenar os sinais das contas, para só realizar as contas quando apertar o '='.
     - Usar um array para armazenar os valores e sinais, depois usar switch e case para realizar as contas. Armazenar o valor escolhido quando apertar o botão de sinal. 
     - Para calculara o resultado, usar find() para saber quantas contas precisamos executar, um switch case dentro de um for detro de um if para calcular todas as contas independente de quantas operações sejam feitas.
 */
