a8c90143da404f1674605b3713bfd635b96a8536
Gui-HR <guilhermehuertar@gmail.com>
2023-06-19 12:10:04 -0300

    Botão Apagar 1.1
    Agora o botão de apagar está funcionado completamente. Foi adicionado a função de porcentagem, mas não está operando corretamente. Também foi adicionada a função de ponto para fazer cálculo com números quebrados.

--- js/script.js
+++ js/script.js
@@ -17,6 +17,7 @@ const numero3= document.querySelector('#numero-3')
 const adicao = document.querySelector('#adicao')
 const parenteses = document.querySelector('#parenteses')
 const numero0 = document.querySelector('#numero-0')
+const ponto = document.querySelector('#ponto')
 const igual = document.querySelector('#igual')
 
 const numeros = [numero0, numero1, numero2, numero3, numero4, numero5, numero6, numero7, numero8, numero9]
@@ -26,42 +27,39 @@ const conta = document.querySelector('#conta')
 const resultado = document.querySelector('#resultado')
 
 // Auxiliares
-var calculo =  []
-var valor = 0
+var calculo = []
+var valor = []
 var quantasContas
-var digitosApagar = []
-var concatenaApagar = []
 var printApagar = []
 var digitosNumero = 0
 
 // Funções
 function apagar () {
 
-    for(let i = 1; i <= (digitosApagar.length - 1); i++) {
-        digitosApagar[0] += digitosApagar[i]
-        digitosApagar[i] = ''
+    for(let i = 1; i <= (valor.length - 1); i++) {
+        valor[0] += valor[i]
+        valor[i] = ''
     }
 
-    digitosApagar = digitosApagar.filter((v) => {
+    valor = valor.filter((v) => {
         return v
     })
 
-    concatenaApagar.push(digitosApagar[0])
+    if(valor[0] != undefined){
+        calculo.push(valor[0])
+    }
 
-    digitosApagar = []
+    valor = []
 }
 
 function operadores (operador) {
-    calculo.push(valor)
     conta.innerHTML += operador
-    valor = 0
-    calculo.push(operador)
     apagar()
-    concatenaApagar.push(operador)
+    calculo.push(operador)
 
     conta.innerHTML = ''
-    for(let i = 0; i <= (concatenaApagar.length - 1); i++) {
-        conta.innerHTML += concatenaApagar[i]
+    for(let i = 0; i <= (calculo.length - 1); i++) {
+        conta.innerHTML += calculo[i]
     }
 
     digitosNumero = 0
@@ -70,49 +68,46 @@ function operadores (operador) {
 for (i in numeros){
     let posicao = i
     numeros[i].addEventListener('click', () => {
-        valor += numeros[posicao].textContent
         conta.innerHTML += numeros[posicao].textContent
-        digitosApagar.push(numeros[posicao].textContent)
+        valor.push(numeros[posicao].textContent)
         digitosNumero += 1
     })
 }
 
+ponto.addEventListener('click', () => {
+    conta.innerHTML += '.'
+    valor.push('.')
+    digitosNumero += 1
+})
+
 ce.addEventListener('click', () => {
     conta.innerHTML = ''
     resultado.innerHTML = ''
-    valor = 0
     calculo = []
-    digitosApagar = []
-    concatenaApagar = []
+    valor = []
     printApagar = []
 })
 
 c.addEventListener('click', () => {
-    console.log(digitosApagar)
 
-    if(digitosApagar[0] == undefined) {
-        console.log('if concatenaApagar: ' + concatenaApagar)
-        concatenaApagar.pop()
-        console.log('if concatenaApagar: ' + concatenaApagar)
+    if(valor[0] == undefined) {
+        calculo.pop()
 
         conta.innerHTML = ''
-        for(i of concatenaApagar) {
+        for(i of calculo) {
             conta.innerHTML += i
         }
 
     } else {
-        digitosApagar.pop()
+        valor.pop()
 
         printApagar = [...conta.innerHTML]
-        console.log(printApagar);
 
         for(let i = 0; i<= (digitosNumero - 1); i++) {
             printApagar.pop()
         }
 
         digitosNumero = digitosNumero - 1
-        console.log(digitosNumero);
-        console.log(printApagar);
         
         conta.innerHTML = ''
         
@@ -120,14 +115,16 @@ c.addEventListener('click', () => {
             conta.innerHTML += i
         }
 
-        for(i of digitosApagar) {
+        for(i of valor) {
             conta.innerHTML += i
         }
-
-        console.log('else digitosApagar ' + digitosApagar)
     }
 
     
+})
+
+porcentagem.addEventListener('click', () => {
+    operadores('%')
 })
 
 adicao.addEventListener('click', () => {
@@ -155,19 +152,17 @@ function limpaArray () {
 }
 
 igual.addEventListener('click', () => {
-    calculo.push(valor)
-    valor = 0
+    apagar()
     calculo = calculo.filter((v) => {
         return v
     })
 
     quantasContas = (calculo.length / 2) + .5
 
-    console.log(calculo);
-
     for(let i = 0; i <= quantasContas; i++){
 
         console.log(calculo)
+        console.log(valor)
 
 
         switch (calculo[1]) {
@@ -190,6 +185,11 @@ igual.addEventListener('click', () => {
                 calculo[0] = String(Number(calculo[0]) / Number(calculo[2]))
                 limpaArray ()
                 break
+
+            case '%':
+                calculo[0] = String((Number(calculo[0]) / 100) * Number(calculo[2]))
+                limpaArray ()
+                break
         
             default:
         }
@@ -202,23 +202,25 @@ igual.addEventListener('click', () => {
 /*
     Fazer:
     - Funcionar usando o teclado.
-    - Arrumar a telinha dos calculos para caber todos os numeros.
-    - Botao de apagar funcionar.
     - As operações seguirem a ordem correta.
+    - Arrumar a telinha dos calculos para caber todos os numeros.
     - Botao de porcentagem funcionar.
-    - Botao de parenteses funcionar
-
+    - Botao de parenteses funcionar.
+    
     Ideias:
+    - As operações seguirem a ordem correta. (Para as operações seguirem a ordem certa podemos, usar um find() para identificar aonde tem os operadores que possuem prioridade, pegar o a posição desses operadores e usando essa posição pegar a posição dos números vizinhos para fazer a conta, depois fazer o procedimento normal para as contas)
     - Para fazer o parenteses criar uma variavel auxiliar e usar um if, se o botão for apertado uma vez vai colocar "(" se for apertado pela segunda vez vai colocar ")".
     - Para o botao de parenteses colocar as contas dentro dele em um array separado, e realizar essas contas antes, depois mandar o resultado para a posição correta.
-    - Para fazer o apagar funcionar criar uma variável auxiliar, que armazena os valores e apaga apagar dessa variavel. Quando o usuário apertar o numero ele vai armazenar digito por digito no 'array secundario', dessa forma se apertar no botão apagar ele vai apagar apenas o ultimo dígito, mas isso muda apos apertar em um sinal de operação, o sinal de operação fará com que concatene todos os digitos e envie para a ultima posição do 'array principal', e botao de operação tambem vai enviar para a ultima posição 'array principal' o seu sinal de operação. (depois disso quando apertar o botão de apagar ele vai apagar todo o numero salvo e não só um digito dele. Posso usar outro array 'auxiliar secundario' para armazenar os digitos e a 'auxiliar principal' para armazenar os numeros completos)
-
-
+    
+    
     Feito:
     - Um jeito de armazenar o valor digitado (não pode ser assim que aperta o botão do dígito, pois o usuário pode querer um número com mais de 1 dígito).
     - Um jeito para armazenar os sinais das contas, para só realizar as contas quando apertar o '='.
     - Usar um array para armazenar os valores e sinais, depois usar switch e case para realizar as contas. Armazenar o valor escolhido quando apertar o botão de sinal. 
     - Para calculara o resultado, usar find() para saber quantas contas precisamos executar, um switch case dentro de um for detro de um if para calcular todas as contas independente de quantas operações sejam feitas.
-*/
-
+    - Botao de apagar funcionar.
+    - Para fazer o apagar funcionar criar uma variável auxiliar, que armazena os valores e apaga apagar dessa variavel. Quando o usuário apertar o numero ele vai armazenar digito por digito no 'array secundario', dessa forma se apertar no botão apagar ele vai apagar apenas o ultimo dígito, mas isso muda apos apertar em um sinal de operação, o sinal de operação fará com que concatene todos os digitos e envie para a ultima posição do 'array principal', e botao de operação tambem vai enviar para a ultima posição 'array principal' o seu sinal de operação, depois disso quando apertar o botão de apagar ele vai apagar todo o numero salvo e não só um digito dele. Posso usar outro array 'auxiliar secundario' para armazenar os digitos e a 'auxiliar principal' para armazenar os numeros completos.
+    - Botao de ponto funcionar.
+    */
+   
 
