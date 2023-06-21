8d532e9f1a30f50dd31e07638917e97616baea86
Gui-HR <guilhermehuertar@gmail.com>
2023-06-21 10:05:12 -0300

    Operações 1.2

--- js/script.js
+++ js/script.js
@@ -54,6 +54,14 @@ function apagar () {
     valor = []
 }
 
+function limpaArray () {
+    calculo[1] = ''
+    calculo[2] = ''
+    calculo = calculo.filter((v) => {
+        return v
+    })
+}
+
 function operadores (operador) {
     conta.innerHTML += operador
     apagar()
@@ -75,7 +83,9 @@ function operações (operador) {
            ordemOperacoesPosicao =  calculo.findIndex(posicao => posicao == operador)
 
             if(ordemOperacoesPosicao != -1){  
-                if(operador == 'x'){
+                if(operador == '%'){
+                    calculo[ordemOperacoesPosicao - 1] = String((Number(calculo[ordemOperacoesPosicao - 1]) / 100) * Number(calculo[ordemOperacoesPosicao + 1]))
+                } else if(operador == 'x'){
                     calculo[ordemOperacoesPosicao - 1] = String(Number(calculo[ordemOperacoesPosicao - 1]) * Number(calculo[ordemOperacoesPosicao + 1]))
                 } else if(operador == '/') {
                     calculo[ordemOperacoesPosicao - 1] = String(Number(calculo[ordemOperacoesPosicao - 1]) / Number(calculo[ordemOperacoesPosicao + 1]))
@@ -94,6 +104,8 @@ function operações (operador) {
         }
     }
 }
+
+// Eventos de click
  
 for (i in numeros){
     let posicao = i
@@ -173,14 +185,6 @@ multiplicacao.addEventListener('click', () => {
     operadores('x')
 })
 
-function limpaArray () {
-    calculo[1] = ''
-    calculo[2] = ''
-    calculo = calculo.filter((v) => {
-        return v
-    })
-}
-
 igual.addEventListener('click', () => {
     apagar()
     calculo = calculo.filter((v) => {
@@ -191,6 +195,7 @@ igual.addEventListener('click', () => {
 
     
     console.log(calculo);
+    operações('%')
 
     operações('x')
 
@@ -209,11 +214,6 @@ igual.addEventListener('click', () => {
                 calculo[0] = String(Number(calculo[0]) - Number(calculo[2]))
                 limpaArray ()
                 break
-
-            case '%':
-                calculo[0] = String((Number(calculo[0]) / 100) * Number(calculo[2]))
-                limpaArray ()
-                break
         
             default:
         }
@@ -225,13 +225,11 @@ igual.addEventListener('click', () => {
 
 /*
     Fazer:
-    - As operações seguirem a ordem correta.
     - Botao de parenteses funcionar.
     - Arrumar a telinha dos calculos para caber todos os numeros.
     - Funcionar usando o teclado.
     
     Ideias:
-    - As operações seguirem a ordem correta. (Para as operações seguirem a ordem certa podemos usar um find() para identificar aonde tem os operadores que possuem prioridade, pegar o a posição desses operadores e usando essa posição pegar a posição dos números vizinhos para fazer a conta, depois fazer o procedimento normal para as contas)
     - Para fazer o parenteses criar uma variavel auxiliar e usar um if, se o botão for apertado uma vez vai colocar "(" se for apertado pela segunda vez vai colocar ")".
     - Para o botao de parenteses colocar as contas dentro dele em um array separado, e realizar essas contas antes, depois mandar o resultado para a posição correta.
     
@@ -240,11 +238,13 @@ igual.addEventListener('click', () => {
     - Um jeito de armazenar o valor digitado (não pode ser assim que aperta o botão do dígito, pois o usuário pode querer um número com mais de 1 dígito).
     - Um jeito para armazenar os sinais das contas, para só realizar as contas quando apertar o '='.
     - Usar um array para armazenar os valores e sinais, depois usar switch e case para realizar as contas. Armazenar o valor escolhido quando apertar o botão de sinal. 
-    - Para calculara o resultado, usar find() para saber quantas contas precisamos executar, um switch case dentro de um for detro de um if para calcular todas as contas independente de quantas operações sejam feitas.
-    - Botao de apagar funcionar.
-    - Para fazer o apagar funcionar criar uma variável auxiliar, que armazena os valores e apaga apagar dessa variavel. Quando o usuário apertar o numero ele vai armazenar digito por digito no 'array secundario', dessa forma se apertar no botão apagar ele vai apagar apenas o ultimo dígito, mas isso muda apos apertar em um sinal de operação, o sinal de operação fará com que concatene todos os digitos e envie para a ultima posição do 'array principal', e botao de operação tambem vai enviar para a ultima posição 'array principal' o seu sinal de operação, depois disso quando apertar o botão de apagar ele vai apagar todo o numero salvo e não só um digito dele. Posso usar outro array 'auxiliar secundario' para armazenar os digitos e a 'auxiliar principal' para armazenar os numeros completos.
+    - Para calcular o resultado. (Usar find() para saber quantas contas precisamos executar, um switch case dentro de um for detro de um if para calcular todas as contas independente de quantas operações sejam feitas.)
+    - Botao de apagar funcionar. (Para fazer o apagar funcionar criar uma variável auxiliar, que armazena os valores e apaga apagar dessa variavel. Quando o usuário apertar o numero ele vai armazenar digito por digito no 'array secundario', dessa forma se apertar no botão apagar ele vai apagar apenas o ultimo dígito, mas isso muda apos apertar em um sinal de operação, o sinal de operação fará com que concatene todos os digitos e envie para a ultima posição do 'array principal', e botao de operação tambem vai enviar para a ultima posição 'array principal' o seu sinal de operação, depois disso quando apertar o botão de apagar ele vai apagar todo o numero salvo e não só um digito dele. Posso usar outro array 'auxiliar secundario' para armazenar os digitos e a 'auxiliar principal' para armazenar os numeros completos.)
     - Botao de ponto funcionar.
     - Botao de porcentagem funcionar.
+    - As operações seguirem a ordem correta.(Para as operações seguirem a ordem certa podemos usar um find() para identificar aonde tem os operadores que possuem prioridade, pegar o a posição desses operadores e usando essa posição pegar a posição dos números vizinhos para fazer a conta, depois fazer o procedimento normal para as contas)
+
+
     */
 
    
